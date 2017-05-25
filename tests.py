f28817b0f7f5b8e9120a1df91a4e7e8b209ee62a
sethbunke <sethbunke@hotmail.com>
2017-05-25 13:27:56 -0700

    testing

--- tests.py
+++ tests.py
@@ -7,8 +7,10 @@ print('peers')
 print(solution.peers)
 """
 
-a = [1,2,3,2,1,5,6,5,5,5]
+input = [1,2,3,2,1,5,6,5,5,5]
 
-result = [item for item, count in collections.Counter(a).items() if count > 1]
+result = [item for item, count in collections.Counter(input).items() if count > 1]
 
-print(result)
+#print(result)
+
+print(solution.peers)
